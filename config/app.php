f5a9ea780acb97e092548ef427c948a500c04d5d
Edgar Ivan Carrasco Galindo <ecarrasco@cideteq.mx>
2022-08-22 16:14:59 -0500

    Acuses 2

--- config/app.php
+++ config/app.php
@@ -165,7 +165,6 @@ return [
         /*
          * Package Service Providers...
          */
-        Barryvdh\DomPDF\ServiceProvider::class,
 
         /*
          * Application Service Providers...
@@ -175,6 +174,7 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        Barryvdh\DomPDF\ServiceProvider::class,
 
     ],
 
